38657ab8bba8802ce56c1f789042f323030ce877
Takaaki Sato <takaaki@demiquartz.jp>
2022-01-30 09:05:25 +0900

    Add library root files

---
+++ include/graphene.hpp
@@ -0,0 +1,64 @@
+/** @file
+ * @brief グラフェンライブラリ
+ * @author Takaaki Sato
+ * @copyright (c) 2022 Demiquartz &lt;info@demiquartz.jp&gt; @n
+ * Distributed under the MIT License (See accompanying file LICENSE
+ * or copy at https://opensource.org/licenses/MIT)
+ */
+#ifndef GRAPHENE_HPP
+#define GRAPHENE_HPP
+
+#include <cstddef>
+
+namespace Graphene {
+
+/**
+ * @brief ライブラリの初期化
+ *
+ * ライブラリを初期化します。 @n
+ * 初期化に失敗した場合は例外を送出します。
+ *
+ * @throw std::exception 初期化失敗
+ * @return なし
+ */
+void Initialize(void);
+
+/**
+ * @brief バージョン情報の取得
+ *
+ * バージョン情報を取得します。
+ *
+ * @return バージョン情報
+ */
+const char* GetVersion(void) noexcept;
+
+/**
+ * @brief メジャーバージョンの取得
+ *
+ * メジャーバージョンを取得します。
+ *
+ * @return メジャーバージョン
+ */
+std::size_t GetMajorVersion(void) noexcept;
+
+/**
+ * @brief マイナーバージョンの取得
+ *
+ * マイナーバージョンを取得します。
+ *
+ * @return マイナーバージョン
+ */
+std::size_t GetMinorVersion(void) noexcept;
+
+/**
+ * @brief パッチバージョンの取得
+ *
+ * パッチバージョンを取得します。
+ *
+ * @return パッチバージョン
+ */
+std::size_t GetPatchVersion(void) noexcept;
+
+} // Graphene
+
+#endif // GRAPHENE_HPP
---
+++ src/graphene.cpp
@@ -0,0 +1,46 @@
+/** @file
+ * @brief グラフェンライブラリ
+ * @author Takaaki Sato
+ * @copyright (c) 2022 Demiquartz &lt;info@demiquartz.jp&gt; @n
+ * Distributed under the MIT License (See accompanying file LICENSE
+ * or copy at https://opensource.org/licenses/MIT)
+ */
+#include <graphene.hpp>
+#include "config.hpp"
+#include "version.hpp"
+
+#ifndef NDEBUG
+#include <iostream>
+#endif
+#if USE_GLFW
+#include "setup/glfw.hpp"
+#endif
+
+namespace Graphene {
+
+void Initialize(void) {
+#ifndef NDEBUG
+    std::clog << SYSNAME << " " << REVISION << std::endl;
+#endif
+#if USE_GLFW
+    Setup::InitializeGLFW();
+#endif
+}
+
+const char* GetVersion(void) noexcept {
+    return VERSION;
+}
+
+std::size_t GetMajorVersion(void) noexcept {
+    return VERSION_MAJOR;
+}
+
+std::size_t GetMinorVersion(void) noexcept {
+    return VERSION_MINOR;
+}
+
+std::size_t GetPatchVersion(void) noexcept {
+    return VERSION_PATCH;
+}
+
+} // Graphene
